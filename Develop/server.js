3e5e7e3579b7e43ad8111336aef12504b74c48ff
Fausto Lopez <lopezfausto123@yahoo.com>
2022-04-28 00:54:56 -0700

    fixed saved side bar

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
 
 // copied paste from activites 22
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 // GET Route for homepage
@@ -17,14 +16,45 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for feedback page
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname + "/db/db.json"), function (err, data){
+    if (err) {
+      console.error(err);
+    } else {
+      const notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  
+  fs.readFile(path.join(__dirname + "/db/db.json"), function (err, data){
+    const notes = JSON.parse(data);
+    const newSave = {
+      title: req.body.title,
+      text: req.body.text,
+      //in order to add multiple saves create new id each time
+      id: Math.random().toString(36).substr(2, 9)
+    };
+    notes.push(newSave);
+    let noteJSON = JSON.stringify(notes);
+    
+    fs.writeFile(path.join(__dirname + "/db/db.json"), noteJSON, (err) => {
+      res.json(noteJSON);
+    })
+  });
+});
+
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
